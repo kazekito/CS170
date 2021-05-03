afefb12c521a889c0bbbb4538d6b6fbd93f15333
Jess-say <lu.jessie1714@gmail.com>
2021-05-02 22:34:37 -0700

    Misplaced tile heuristic

--- misplaced.cpp
+++ misplaced.cpp
@@ -1,63 +1,33 @@
 #include "misplaced.h"
+#include "node.h"
 
-Node::Node() {
-	paren = vector<vector<vector<int>>>();
-	vect = vector<vector<int>>();
 
-}
-Tree::Tree() {
+misplace::misplace() {
 	this->root = Node();
 	this->goal_vector = vector<Node>();
 	this->frontier = list<Node>();
 	this->explored = vector<Node>();
 }
-void Tree::set_root(Node a) {
-	this->root = a;
+
+
+void misplace::set_root(Node add) {
+	this->root = add;
 }
 
-void Tree::add_frontier(Node add) {
+void misplace::add_frontier(Node add) {
 	this->frontier.push_back(add);
 }
 
-void Tree::add_explored(Node add) {
+void misplace::add_explored(Node add) {
 	this->explored.push_back(add);
 }
 
-void Tree::add_goal(Node add) {
+void misplace::add_goal(Node add) {
 	this->goal_vector.push_back(add);
 }
 
-void Tree::remove_frontier() {
-	this->frontier.pop_front();
-}
+bool misplace::compare_goal(Node add) {
 
-void Tree::check_misplace(Node add) {
-	vector<vector<int>> temp{
-		{1,2,3},
-		{4,5,6},
-		{7,8,0}
-	};
-
-	misplace = 0;
-
-	if (add.vect == temp) {
-		add.tile.push_back(misplace);
-	}
-	else {
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) {
-				if (temp.at(i).at(j) != add.vect.at(i).at(j)) {
-					misplace++;
-				}
-			}
-		}
-		add.tile.push_back(misplace);
-	}
-}
-
-
-bool Tree::compare_goal(Node add) {	//check if our Node add is the same as our goal state, if not 
-	bool tru = true;
 	vector<vector<int>> temp{
 		{1,2,3},
 		{4,5,6},
@@ -70,14 +40,12 @@ bool Tree::compare_goal(Node add) {	//check if our Node add is the same as our g
 	else {
 		return false;
 	}
-
-	return tru;
 }
 
-bool Tree::isExplored(Node add) { //execute compare add through our explored set. the logic is that if it is true, then tru = true. if it doesn't match, tru = false for the rest of the comparison. 
-	bool temp = true;
+bool misplace::isExplored(Node add) {
+
 	if (this->explored.empty()) {
-		temp = false;
+		return false;
 	}
 	else {
 		for (int i = 0; i < this->explored.size(); ++i) {
@@ -85,289 +53,336 @@ bool Tree::isExplored(Node add) { //execute compare add through our explored set
 				return true;
 			}
 		}
-		temp = false;
+		return false;
 	}
-
-	return temp;
 }
 
-void Tree::expand_left(Node& add) {
-	Node temp = add;
-	int a; int b;
+bool misplace::isFrontier(Node add) {
+
+	list<Node>::iterator it;
+	for (it = this->frontier.begin(); it != this->frontier.end(); ++it) {
+		if (it->vect == add.vect) {
+			return true;
+		}
+	}
+	return false;
+}
+
+void misplace::remove_frontier(Node add) {
+	list<Node>::iterator it;
+	for (it = this->frontier.begin(); it != this->frontier.end(); ++it) {
+		if (it->vect == add.vect) {
+			this->frontier.erase(it);
+			return;
+		}
+	}
+	return;
+}
+
+void misplace::expand_left(Node* add) {
+	Node temp = *add;
+	int a; 
+	int b;
+
 	a = temp.vect.at(temp.a).at(temp.b);  //get # for blank "b"
 	b = temp.vect.at(temp.a).at(temp.b - 1); //get # for position left of blank
 	temp.vect.at(temp.a).at(temp.b - 1) = a; //swap blank and # to the left of blank.
 	temp.vect.at(temp.a).at(temp.b) = b;
 
-	//++temp.cost; //increase cost of our state.
-	//temp.cost = misplace + depth;
 	temp.b = temp.b - 1; //change columns by 1 value to the left/
-	temp.child_list = vector<Node>();  //initalize an empty child list for our newly expanded set.
 
-	add.child_list.push_back(temp);
-	temp.paren.push_back(add.vect);
+	temp.child_list = vector<Node>();  //initalize an empty child list for our newly expanded set.
+	add->child_list.push_back(temp);
+
+	temp.paren.push_back(add->vect);
+	temp.parent_vector.push_back(*add);
 
 	temp.dir.push_back("Move left");
-	
-	temp.dep.push_back(depth);
 
-	check_misplace(temp);
-	temp.tile.push_back(misplace);
+	calc_misplace(&temp);
 
-	if (compare_goal(temp)) {
-		add_goal(temp);
-	}
-
-	if (!isExplored(temp)) {
+	if (!isExplored(temp) && !isFrontier(temp)) {
 		add_frontier(temp);
 	}
 
 }
 
-void Tree::expand_up(Node& add) {
-	Node temp = add;
-	int a; int b;
-	a = temp.vect.at(temp.a).at(temp.b);  //get # for blank "b"
-	b = temp.vect.at(temp.a - 1).at(temp.b); //get # for position above blank
-	temp.vect.at(temp.a - 1).at(temp.b) = a; //swap blank and # to the above blank.
-	temp.vect.at(temp.a).at(temp.b) = b;
+void misplace::expand_right(Node* add) {
+	Node temp = *add;
+	int a;
+	int b;
 
-	++temp.cost; //increase cost of our state.
-	temp.a = temp.a - 1; //change columns by 1 value to the left/
-	temp.child_list = vector<Node>();  //initalize an empty child list for our newly expanded set.
-
-	add.child_list.push_back(temp);
-	temp.paren.push_back(add.vect);
-	temp.dir.push_back("Move up");
-	
-	temp.dep.push_back(depth);
-
-	check_misplace(temp);
-	temp.tile.push_back(misplace);
-
-	if (compare_goal(temp)) {
-		add_goal(temp);
-	}
-
-	if (!isExplored(temp)) {
-		add_frontier(temp);
-	}
-}
-
-void Tree::expand_down(Node& add) {
-	Node temp = add;
-	int a; int b;
-	a = temp.vect.at(temp.a).at(temp.b);  //get # for blank "b"
-	b = temp.vect.at(temp.a + 1).at(temp.b); //get # for position underneath  blank
-	temp.vect.at(temp.a + 1).at(temp.b) = a; //swap blank and # to the underneath blank.
-	temp.vect.at(temp.a).at(temp.b) = b;
-
-	++temp.cost; //increase cost of our state.
-	temp.a = temp.a + 1; //change columns by 1 value to the left/
-	temp.child_list = vector<Node>();  //initalize an empty child list for our newly expanded set.
-
-	add.child_list.push_back(temp);
-	temp.paren.push_back(add.vect);
-	temp.dir.push_back("Move down");
-	
-	temp.dep.push_back(depth);
-
-	check_misplace(temp);
-	temp.tile.push_back(misplace);
-
-	if (compare_goal(temp)) {
-		add_goal(temp);
-		
-	}
-
-	if (!isExplored(temp)) {
-		add_frontier(temp);
-	}
-}
-
-void Tree::expand_right(Node& add) {
-	Node temp = add;
-	int a; int b;
 	a = temp.vect.at(temp.a).at(temp.b);  //get # for blank "b"
 	b = temp.vect.at(temp.a).at(temp.b + 1); //get # for position right of blank
 	temp.vect.at(temp.a).at(temp.b + 1) = a; //swap blank and # to the right of blank.
 	temp.vect.at(temp.a).at(temp.b) = b;
 
-	++temp.cost; //increase cost of our state.
-	temp.b = temp.b + 1; //change columns by 1 value to the right/
-	temp.child_list = vector<Node>();  //initalize an empty child list for our newly expanded set.
+	temp.b = temp.b + 1; //change columns by 1 value to the right
 
-	add.child_list.push_back(temp);
-	temp.paren.push_back(add.vect);
+	temp.child_list = vector<Node>();  //initalize an empty child list for our newly expanded set.
+	add->child_list.push_back(temp);	//add this node to add's child list.
+
+	temp.paren.push_back(add->vect);
+	temp.parent_vector.push_back(*add);
+
 	temp.dir.push_back("Move right");
 
-	temp.dep.push_back(depth);
+	calc_misplace(&temp);
 
-	check_misplace(temp);
-	temp.tile.push_back(misplace);
-
-	if (compare_goal(temp)) {
-		add_goal(temp);
-		
-	}
-
-	if (!isExplored(temp)) {
+	if (!isExplored(temp) && !isFrontier(temp)) {
 		add_frontier(temp);
 	}
 }
 
-void Tree::expand(Node add) {
-	int a = add.a;
-	int b = add.b;
-	if (compare_goal(add)) {
-		add_goal(add);
+void misplace::expand_up(Node* add) {
+	Node temp = *add;
+	int a; 
+	int b;
+
+	a = temp.vect.at(temp.a).at(temp.b);  //get # for blank "b"
+	b = temp.vect.at(temp.a - 1).at(temp.b); //get # for position above blank
+	temp.vect.at(temp.a - 1).at(temp.b) = a; //swap blank and # to the above blank.
+	temp.vect.at(temp.a).at(temp.b) = b;
+
+	temp.a = temp.a - 1; //change columns by 1 value to the left
+
+	temp.child_list = vector<Node>();  //initalize an empty child list for our newly expanded set.
+	add->child_list.push_back(temp);
+
+	temp.paren.push_back(add->vect);
+	temp.parent_vector.push_back(*add);
+
+	temp.dir.push_back("Move up");
+
+	calc_misplace(&temp);
+
+	if (!isExplored(temp) && !isFrontier(temp)) {
+		add_frontier(temp);
+	}
+}
+
+void misplace::expand_down(Node* add) {
+	Node temp = *add;
+	int a; 
+	int b;
+
+	a = temp.vect.at(temp.a).at(temp.b);  //get # for blank "b"
+	b = temp.vect.at(temp.a + 1).at(temp.b); //get # for position underneath  blank
+	temp.vect.at(temp.a + 1).at(temp.b) = a; //swap blank and # to the underneath blank.
+	temp.vect.at(temp.a).at(temp.b) = b;
+
+	temp.a = temp.a + 1; //change columns by 1 value to the left
+
+	temp.child_list = vector<Node>();  //initalize an empty child list for our newly expanded set.
+	add->child_list.push_back(temp);
+
+	temp.paren.push_back(add->vect);
+	temp.parent_vector.push_back(*add);
+
+	temp.dir.push_back("Move down");
+
+	calc_misplace(&temp);
+
+	if (!isExplored(temp) && !isFrontier(temp)) {
+		add_frontier(temp);
+	}
+
+}
+
+void misplace::expand(Node a) {
+	int tempa = a.a;
+	int tempb = a.b;
+
+	if (compare_goal(a)) {
+		add_goal(a);
 		if (this->frontier.size() == 1) {
 			this->frontier.front() = Node();
 		}
-		else {
-			this->frontier.pop_front();
+
+	}
+	else if (!isExplored(a) && !compare_goal(a)) {
+		if (tempa == 0 && tempb == 0) { //blank is at top left corner.
+			add_explored(a);
+			expand_right(&a);
+			expand_down(&a);
+		}
+		else if (tempa == 0 && tempb == 1) { //blank is at the top corner
+			add_explored(a);
+			expand_right(&a);
+			expand_down(&a);
+			expand_left(&a);
+		}
+		else if (tempa == 0 && tempb == 2) { //blank is at top right corner
+			add_explored(a);
+			expand_left(&a);
+			expand_down(&a);
+		}
+		else if (tempb == 0 && tempa == 1) { //blank is at the left corner.
+			add_explored(a);
+			expand_right(&a);
+			expand_up(&a);
+			expand_down(&a);
+		}
+		else if (tempb == 0 && tempa == 2) {  //blank is at the bottom left
+			add_explored(a);
+			expand_up(&a);
+			expand_right(&a);
+		}
+		else if (tempa == 2 && tempb == 1) { //blank is at the bottom corner
+			add_explored(a);
+			expand_up(&a);
+			expand_left(&a);
+			expand_right(&a);
+		}
+		else if (tempa == 2 && tempb == 2) { //bottom right corner.
+			add_explored(a);
+			expand_up(&a);
+			expand_left(&a);
+		}
+		else if (tempb == 2 && tempa == 1) { //right corner.
+			add_explored(a);
+			expand_left(&a);
+			expand_up(&a);
+			expand_down(&a);
+		}
+		else if (tempb == 1 && tempa == 1) {
+			add_explored(a);
+			expand_left(&a);
+			expand_right(&a);
+			expand_up(&a);
+			expand_down(&a);
 		}
 	}
-	else if (!isExplored(add) && !compare_goal(add)) {
-		if (a == 0 && b == 0) { //blank is at top left corner.
-			add_explored(add);
-			expand_right(add);
-			expand_down(add);
-		}
-		else if (a == 0 && b == 1) { //blank is at the top corner
-			add_explored(add);
-			expand_right(add);
-			expand_down(add);
-			expand_left(add);
-		}
-		else if (a == 0 && b == 2) { //blank is at top right corner
-			add_explored(add);
-			expand_left(add);
-			expand_down(add);
-		}
-		else if (b == 0 && a == 1) { //blank is at the left corner.
-			add_explored(add);
-			expand_right(add);
-			expand_up(add);
-			expand_down(add);
-		}
-		else if (b == 0 && a == 2) {  //blank is at the bottom left
-			add_explored(add);
-			expand_up(add);
-			expand_right(add);
-		}
-		else if (a == 2 && b == 1) { //blank is at the bottom corner
-			add_explored(add);
-			expand_up(add);
-			expand_left(add);
-			expand_right(add);
-		}
-		else if (a == 2 & b == 2) { //bottom right corner.
-			add_explored(add);
-			expand_up(add);
-			expand_left(add);
-		}
-		else if (b == 2 && a == 1) { //right corner.
-			add_explored(add);
-			expand_left(add);
-			expand_up(add);
-			expand_down(add);
-		}
-		else if (b == 1 && a == 1) {
-			add_explored(add);
-			expand_left(add);
-			expand_right(add);
-			expand_up(add);
-			expand_down(add);
-		}
-	}
-	depth++;
-	this->frontier.pop_front();
+
+	remove_frontier(a);
 }
 
-void Tree::expand_frontier() {
+void misplace::expand_frontier() {
 	while (this->goal_vector.empty()) {
-		print_frontier_front();
-		print_frontier_size();
-		print_expanded_size();
-		expand(this->frontier.front());
+		print_node(get_lowestcost());
+		expand(get_lowestcost());
+		goal_exist();
 	}
 }
 
+Node misplace::get_lowestcost() {
 
-//*******************************************************testing purposes ***********************************************************************************************************************
-
-void Tree::print_frontier_front() {
-	if (!this->frontier.empty()) {
-		for (int i = 0; i < 3; ++i) {
-			cout << "{ ";
-			for (int j = 0; j < 3; ++j) {
-				cout << this->frontier.front().vect.at(i).at(j) << ", ";
-			}
-			cout << "}\n";
+	list<Node>::iterator it;
+	Node temp;
+	temp = frontier.front();
+	for (it = this->frontier.begin(); it != this->frontier.end(); ++it) {
+		if (it->Tcost < temp.Tcost) {
+			temp = *it;
 		}
 	}
-
-	cout << endl;
+	return temp;
 }
 
-void Tree::GoalExist() {
-	if (this->goal_vector.empty()) {
-		cout << "NO" << endl;
+bool misplace::position_checker(double value, double i, double j) {
+
+	// check if value matches one from goal state
+	if (value == 1 && i == 0 && j == 0) {
+		return true;
+	}
+	else if (value == 2 && i == 0 && j == 1) {
+		return true;
+	}
+	else if (value == 3 && i == 0 && j == 2) {
+		return true;
+	}
+	else if (value == 4 && i == 1 && j == 0) {
+		return true;
+	}
+	else if (value == 5 && i == 1 && j == 1) {
+		return true;
+	}
+	else if (value == 6 && i == 1 && j == 2) {
+		return true;
+	}
+	else if ((value == 7 && i == 2 && j == 0)) {
+		return true;
+	}
+	else if (value == 8 && i == 2 && j == 1) {
+		return true;
+	}
+	else if (value == 0 && i == 2 && j == 2) {
+		return true;
 	}
 	else {
-		cout << "YES" << endl;
+		return false;
 	}
+
+	return false;
 }
 
-void Tree::print_goal_cost() {
+void misplace::calc_misplace(Node* a) {
+	int count = 0;
+
+	for (int i = 0; i < 3; ++i) {
+		for (int j = 0; j < 3; ++j) {
+			if (!position_checker(a->vect.at(i).at(j), i, j)) {
+				count = count + 1;
+			}
+		}
+	}
+	a->Hcost = count;
+	a->Tcost = a->cost + count;
+}
+
+void misplace::goal_exist() {
 	if (!this->goal_vector.empty()) {
-		printf("Goal cost %d\n", this->goal_vector.at(0).cost);
+		cout << "Goal!" << endl;
+		Node temp = this->goal_vector.at(0);
+		print_node(temp);
 	}
 }
 
-
-void Tree::print_solution() {
-	cout << "Solution: " << endl;
+void misplace::print_directions() {
 	Node temp;
+
 	if (!this->goal_vector.empty()) {
 		temp = goal_vector.at(0);
-		for (int i = 0; i < temp.paren.size(); ++i) {
-			// keep track of depth and misplace			depth doesn't exactly work
-			cout << "The best state to expand with g(n) = " << i/*temp.dep.at(i)*/ << " and h(n) = " << temp.tile.at(i) << endl; 
-			print_vector(temp.paren.at(i));
+		cout << endl << endl << "***Solution***" << endl;
+
+		for (int i = 0; i < temp.parent_vector.size(); ++i) {
+			print_node(temp.parent_vector.at(i));
 		}
-		print_vector(temp.vect);
-		cout << "Directions\n";
-		print_vector(this->root.vect);
+		print_node(temp);
+
+		cout << endl << "***Directions***" << endl;
 		for (int j = 0; j < temp.dir.size(); ++j) {
 			cout << temp.dir.at(j) << endl;
 		}
 	}
-
-
 }
 
-
-void Tree::print_vector(vector<vector<int>> add) {
-	for (int i = 0; i < 3; ++i) {
+void misplace::print_vector(vector<vector<int>> a) {
+	for (int i = 0; i < 3; i++) {
 		cout << "{ ";
-		for (int j = 0; j < 3; ++j) {
-			cout << add.at(i).at(j) << ", ";
+		for (int j = 0; j < 3; j++) {
+			cout << a.at(i).at(j);
 		}
 		cout << "}\n";
 	}
-	cout << "\n";
+	cout << endl;
 }
 
-void Tree::print_frontier_size() {
-	if (!this->frontier.empty()) {
-		cout << "Frontier: " << this->frontier.size() << " ";
-	}
+void misplace::print_T(Node a) {
+	cout << "Total cost: " << a.Tcost << endl;;
 }
 
-void Tree::print_expanded_size() {
-	if (!this->explored.empty()) {
-		cout << "Expanded: " << this->explored.size() << " \n";
+void misplace::print() {
+	cout << this->frontier.front().Tcost << endl;
+}
+
+void misplace::print_node(Node a) {
+	for (int i = 0; i < 3; ++i) {
+		cout << "{ ";
+		for (int j = 0; j < 3; ++j) {
+			cout << a.vect.at(i).at(j) << ", ";
+		}
+		cout << "}" << endl;
 	}
+	cout << endl;
+	cout << "g(n): " << a.cost << "\t h(n): " << a.Hcost << "\t f(n): " << a.Tcost << endl;
 }
